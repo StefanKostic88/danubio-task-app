26756262d26995549c0400a24e14249de82a5cc2
stefanKostic88 <stefankostic88@yahoo.com>
2023-05-14 10:49:13 +0200

    assets created

---
+++ src/assets/styles/componentStyles/navigationStyles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const NavigationStyled = styled.nav`
+  // background: #e0e0e0;
+`;
+
+export const InnerNavStyled = styled.div`
+  max-width: ${({ theme }) => theme.width.xl};
+  padding: 1.75rem 0;
+  display: flex;
+  margin: 0 auto;
+  @media ${({ theme }) => theme.breakpoints.xl} {
+    padding: 1.75rem 2rem;
+  }
+`;
+
+export const NavigationHeadingStyled = styled.h3`
+  // letter-spacing: 0.25px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: all 300ms ease;
+  &:hover {
+    transition: all 300ms ease;
+    color: ${({ theme }) => theme.color.primaryGreen};
+  }
+  @media ${({ theme }) => theme.breakpoints.mobile} {
+    text-align: center;
+    width: 100%;
+  }
+`;
---
+++ src/assets/theme/Theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  fontColor: {
+    primary: "#fff",
+    primaryDark: "#000",
+  },
+  color: {
+    primaryGreen: "#188754",
+    primaryBlue: "#0C6EFD",
+  },
+  width: { xl: "1300px", lg: "1075px" },
+  breakpoints: {
+    xl: `(max-width: 1300px)`,
+    lg: `(max-width: 1075px),`,
+    md: "(max-width: 775px)",
+    sm: "(max-width: 550px)",
+    xsm: "(max-width: 450px)",
+    mobile: "(max-width: 370px)",
+  },
+};
+
+export default theme;
